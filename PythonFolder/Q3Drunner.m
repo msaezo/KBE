0b458257f5f31b5b8c045244c9b2ae3438d4ed28
Miguel Angel Saez <48205323+msaezo@users.noreply.github.com>
2021-04-22 02:59:55 +0200

    Change cst coefficients

--- PythonFolder/Q3Drunner.m
+++ PythonFolder/Q3Drunner.m
@@ -11,8 +11,8 @@ AC.Wing.inc  = 0;
 
 % Airfoil coefficients input matrix
 %                    | ->     upper curve coeff.  <-|  | ->  lower curve coeff.       <-| 
-AC.Wing.Airfoils = [0.2093 0.3046 0.2407 0.2846 0.2217 -0.1305 -0.1404 -0.0146 -0.0088 0.0447;
-                    0.3408 0.3074 0.3133 0.2536 0.3276 -0.3408 -0.3074 -0.3133 -0.2536 0.3276];
+AC.Wing.Airfoils = [0.2093 0.3046 0.2407 0.2846 0.2217 -0.1305 -0.1404 -0.0146 -0.0088 -0.0447;
+                    0.3407 0.3078 0.3127 0.2541 0.3275 -0.3407 -0.3078 -0.3127 -0.2541 -0.3275];
                 
 AC.Wing.eta = [0;1];  % Spanwise location of the airfoil sections
 
