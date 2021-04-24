b7c755c4bd05a8f4c7f7b9125676af21780f9e9d
Miguel Angel Saez <48205323+msaezo@users.noreply.github.com>
2021-04-24 02:03:25 +0200

    Main created, and update on cg for hydrogen tanks

---
+++ PythonFolder/Main.py
@@ -0,0 +1,28 @@
+# This file is supposed to:
+# 1. select the input file (the excel file)
+# 2. call Aircraft Geometry
+# 3. Check whether "troublemaker" parameters meet the expected values (if problems, warnings should pop)
+# 4. If they do meet the expected values, create an output file with CAD, input parameters and some key parameters
+# 5. If they do not meet the expected values:
+#   5.a. Take action depending on which "troublemaker" parameter failed
+#   5.b. Run Aircraft Geometry
+#   5.c. Go back to step 3 and repeat
+
+# "Troublemaker" Parameters:
+#   - Cd: Nan               -- Action:
+#       - Decrease flight altitude          (might not be good)
+#       - Increase wing area                (might not be good)
+#       - Assume L/D ratio of 20            (best solution till now)
+#       - WARNING: -Nan detected, action point taken -
+#   - Tank diameter > value -- Action       (the value should be the one we have currently, more modifies too much fus)
+#       - Decrease range                    (good option, but not preferable)
+#       - Give options to user              (input different airfoil,...)
+#       - WARNING: - Tank diameter too big, action point taken -
+#   - C.G.Range Hydrogen  > C.G. Range Kerosene   -- Just give warning
+#       - WARNING: - Problems might arise due to higher C.G. range than before -
+#   - Any other?
+
+# Some other doubts I have at the moment, or improvements for future.
+#   - Include the drag of the fuselage, theoretically we could extend it and drag would not increase (good that tank is limited by the fus length)
+#   - Fuel weight defined twice? once in fuel mass fraction (guessing kerosene) twice range (guessing hydrogen)
+#   - Cargo not included at all
--- PythonFolder/aircraft/cg_calculations.py
+++ PythonFolder/aircraft/cg_calculations.py
@@ -10,7 +10,7 @@ from aircraft.wing import Wing
 
 
 
-
+#Kerosene Based CG
 class CG_calculations(GeomBase):
 
     payload_cg_loc = Input(I.Payload_cg_loc)
@@ -43,6 +43,72 @@ class CG_calculations(GeomBase):
         OEW_and_payload = (self.x_oew*self.mass_oew + self.x_payload*self.mass_payload)/(self.mass_oew + self.mass_payload)
         OEW_and_paylod_and_fuel = (self.x_fuel*self.mass_fuel + self.x_oew*self.mass_oew + self.x_payload*self.mass_payload)/(self.mass_fuel + self.mass_oew + self.mass_payload)
         OEW_and_fuel = (self.x_oew*self.mass_oew + self.x_fuel*self.mass_fuel)/(self.mass_fuel + self.mass_oew)
+        return max(OEW_and_fuel, OEW_and_paylod_and_fuel, OEW_and_payload)
+
+    @Part
+    def cg_front(self):
+        return LineSegment(start=Point(self.cg_forward, -4, 0),
+                           end=Point(self.cg_forward, 4, 0),
+                           color='green',
+                           line_thickness=2)
+
+    @Part
+    def cg_rear(self):
+        return LineSegment(start=Point(self.cg_aft, -4, 0),
+                           end=Point(self.cg_aft, 4, 0),
+                           color='green',
+                           line_thickness=2)
+
+
+
+#Hydrogen Based CG  ---- Not sure if imports are correct (hardcoded obviously not) but calculations are.
+#Maybe place in a different file so we only run it if necessary.
+class CG_calculations(GeomBase):
+    payload_cg_loc = Input(I.Payload_cg_loc)
+    fuel_cg_loc = # here should be the middle point of the tanks
+    tank_cg_loc = # here should be the middle point of the tanks
+    mass_fuel   = # output from range calculation
+    g_i         = Input(0.5) # gravimetric index taken from report on flying V, cryogenic tank
+    mass_oew = Input(I.OEW_mass_fraction)
+    mass_payload = Input(I.Payload_mass_fraction)
+
+    @Attribute
+    def mass_tank(self):
+        return (self.mass_fuel-self.g_i*self.mass_fuel)/self.g_i
+
+    @Attribute           # Are we missing in this function the weight of the fuselage? or it is included?
+    def x_oew(self):     #  HERE I ADD THE CENTER OF GRAVITY CONTRIBUTION OF THE TANK WEIGHT
+        return ((Wing().x_le_mac+0.25*Wing().mean_aerodynamic_chord)*self.mass_oew + self.mass_tank *\
+                self.tank_cg_loc)/(self.mass_oew + self.mass_tank)
+
+    @Attribute
+    def x_payload(self):
+        return self.payload_cg_loc * Fuselage().length_fuselage
+
+    @Attribute
+    def x_fuel(self):
+        return Wing().x_le_mac + self.fuel_cg_loc * Wing().mean_aerodynamic_chord
+
+    @Attribute
+    def cg_forward(self):
+        OEW_and_payload = (self.x_oew * self.mass_oew + self.x_payload * self.mass_payload) / (
+                    self.mass_oew + self.mass_payload)
+        OEW_and_payload_and_fuel = (
+                                               self.x_fuel * self.mass_fuel + self.x_oew * self.mass_oew + self.x_payload * self.mass_payload) / (
+                                               self.mass_fuel + self.mass_oew + self.mass_payload)
+        OEW_and_fuel = (self.x_oew * self.mass_oew + self.x_fuel * self.mass_fuel) / (
+                    self.mass_fuel + self.mass_oew)
+        return min(OEW_and_fuel, OEW_and_payload_and_fuel, OEW_and_payload)
+
+    @Attribute
+    def cg_aft(self):
+        OEW_and_payload = (self.x_oew * self.mass_oew + self.x_payload * self.mass_payload) / (
+                    self.mass_oew + self.mass_payload)
+        OEW_and_paylod_and_fuel = (
+                                              self.x_fuel * self.mass_fuel + self.x_oew * self.mass_oew + self.x_payload * self.mass_payload) / (
+                                              self.mass_fuel + self.mass_oew + self.mass_payload)
+        OEW_and_fuel = (self.x_oew * self.mass_oew + self.x_fuel * self.mass_fuel) / (
+                    self.mass_fuel + self.mass_oew)
         return max(OEW_and_fuel, OEW_and_paylod_and_fuel, OEW_and_payload)
 
     @Part
