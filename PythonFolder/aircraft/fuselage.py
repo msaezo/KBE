fixed units of step file

PythonFolder/aircraft/fuselage.py
@@ -180,7 +180,8 @@ class Fuselage(GeomBase):
     # creating fuselage sections and placing them at desired location
     @Part
     def inner_profile_set(self):
-        return Circle(quantify=len(self.fuselage_sections), color="Black",
+        return Circle(quantify=len(self.fuselage_sections),
+                      color="Black",
                       radius=self.section_radius_inner[child.index],
                       # fuselage along the X axis, nose in XOY
                       position=translate(
@@ -269,18 +270,18 @@ class Fuselage(GeomBase):
     @Attribute
     def n_rows_middle(self):
         return int(np.floor((self.length_fuselage - self.length_nosecone - self.length_tailcone)
-                                             / (0.9 * self.k_cabin)))
+                            / (0.9 * self.k_cabin)))
 
     @Attribute
     def n_rows_rear(self):
         return int((self.n_pax - self.seats_abreast * int(np.floor((self.length_fuselage
-                                                                                     - self.length_nosecone
-                                                                                     - self.length_tailcone)
-                                                                                    / (0.9 * self.k_cabin)))
-                                     - (self.seats_abreast - 2) * int(np.floor((self.length_nosecone
-                                                                                - self.length_cockpit)
-                                                                               / (0.8 * self.k_cabin))))
-                                    / (self.seats_abreast - 2))
+                                                                    - self.length_nosecone
+                                                                    - self.length_tailcone)
+                                                                   / (0.9 * self.k_cabin)))
+                    - (self.seats_abreast - 2) * int(np.floor((self.length_nosecone
+                                                               - self.length_cockpit)
+                                                              / (0.8 * self.k_cabin))))
+                   / (self.seats_abreast - 2))
 
     # Place seats in front part (less seats abreast as fuselage is slimming down)
     # Uses seat row class
@@ -325,7 +326,8 @@ class Fuselage(GeomBase):
                        quantify=self.n_rows_rear,
                        position=translate(self.position,
                                           'x',
-                                          self.length_fuselage-self.length_tailcone + child.index * 0.8 * self.k_cabin),
+                                          self.length_fuselage
+                                          - self.length_tailcone + child.index * 0.8 * self.k_cabin),
                        hidden=False)
 
 